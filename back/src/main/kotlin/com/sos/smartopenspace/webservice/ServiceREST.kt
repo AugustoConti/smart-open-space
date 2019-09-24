b3a3176dfc1d6532796abb8dcb489a8847ca36de
AugustoConti <augustoconti10@gmail.com>
2019-09-23 23:53:58 -0300

    :gem: Agrego heroku

--- back/src/main/kotlin/com/sos/smartopenspace/webservice/ServiceREST.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/webservice/ServiceREST.kt
@@ -4,5 +4,5 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["http://localhost:1234"])
+@CrossOrigin(origins = ["http://localhost:1234", "https://smartopenspace.herokuapp.com"])
 annotation class ServiceREST
