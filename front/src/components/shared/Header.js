32030bb1fe415816a54d2f701fc5d667ed437413
AugustoConti <augustoconti10@gmail.com>
2019-09-30 00:52:49 -0300

    :alien: Agrego alias

--- front/src/components/shared/Header.js
+++ front/src/components/shared/Header.js
@@ -8,7 +8,7 @@ import RowBetween from './RowBetween';
 import useAuth, { useUser } from '#helpers/useAuth';
 import useSize from '#helpers/useSize';
 
-import logo from '#dist/logo.svg';
+const logo = require('#dist/logo.svg');
 
 const SmallMenu = ({ color }) => (
   <Box direction="row" pad="medium" justify="end">
@@ -27,6 +27,12 @@ LargeMenu.propTypes = {
   color: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
+
+const LogoSmall = () => (
+  <Box height="xxsmall" width="xxsmall" round="small" pad="xxsmall" background="accent-1">
+    <Image fit="cover" src={logo} />
+  </Box>
+);
 
 const Header = ({ history }) => {
   const size = useSize();
@@ -56,18 +62,6 @@ const Header = ({ history }) => {
       },
     },
   ];
-
-  const LogoSmall = () => (
-    <Box
-      height="xxsmall"
-      width="xxsmall"
-      round="small"
-      pad="xxsmall"
-      background="accent-1"
-    >
-      <Image fit="cover" src={logo} />
-    </Box>
-  );
 
   const ButtonHome = () => (
     <Button
