a600706ffe69de527b6781a3b1e39aa72468615d
AugustoConti <augustoconti10@gmail.com>
2019-11-18 14:19:29 -0300

    :bug: Fix migration db queue state

--- back/src/main/resources/db/migration/V1_3__create_queue_state.sql
+++ back/src/main/resources/db/migration/V1_3__create_queue_state.sql
@@ -7,6 +7,7 @@ DO $$
               AND    table_name = 'open_space'
             )
         THEN
+            ALTER TABLE open_space ADD COLUMN queue_state varchar(255);
             UPDATE open_space SET queue_state = 'ACTIVE' WHERE is_active_queue = TRUE;
             UPDATE open_space SET queue_state = 'PENDING' WHERE is_active_queue = FALSE;
             ALTER TABLE open_space DROP COLUMN IF EXISTS is_active_queue;
