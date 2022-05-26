f9f26c420e91c644e8344c48cf6a34445ae1568d
Francisco De Grandis <francisco.degrandis@10pines.com>
2022-05-26 11:03:53 -0300

    Agregar migracion para la creacion de tracks

---
+++ back/src/main/resources/db/migration/V1_7__create_tracks.sql
@@ -0,0 +1,10 @@
+create table if not exists track
+(
+    id bigint not null,
+    name   varchar(100) not null,
+    description   varchar(500),
+    color   varchar(7),
+    open_space_id bigint,
+    primary key (id),
+    foreign key (open_space_id) references open_space(id)
+    );
