8b2d6659df0fabb874f1074e30a5c1d929ab0be3
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-06-03 10:51:41 -0300

    Track es una data class ahora

--- back/src/main/kotlin/com/sos/smartopenspace/domain/Track.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/domain/Track.kt
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
 
 @Entity
-class Track(
+data class Track(
     @field:NotEmpty
     val name: String,
 
@@ -23,22 +23,4 @@ class Track(
     @JsonIgnore
     @OneToMany(mappedBy = "track", cascade = [CascadeType.ALL])
     val talks: MutableSet<Talk> = mutableSetOf()
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null) return false
-
-        val otherTalk: Track = other as Track
-
-        return otherTalk.name == this.name &&
-                otherTalk.color == this.color &&
-                otherTalk.description == this.description
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + description.hashCode()
-        result = 31 * result + color.hashCode()
-        result = 31 * result + id.hashCode()
-        return result
-    }
 }
