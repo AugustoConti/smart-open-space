87bd3fd3579fca9271dd94f946b6738613536d68
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-06-14 13:03:50 -0300

    Modificado el Undo para dates de slots para tolerar la subquery con diferentes fechas

--- back/src/main/resources/db/migration/U1_10__add_dates_to_slots.sql
+++ back/src/main/resources/db/migration/U1_10__add_dates_to_slots.sql
@@ -2,7 +2,7 @@ alter table open_space
     add column if not exists date date;
 
 update open_space
-set date=(select slot.date from slot where open_space.id = slot.open_space_id)
+set date=(select slot.date from slot where open_space.id = slot.open_space_id LIMIT 1)
 where open_space.date is null;
 
 alter table slot
