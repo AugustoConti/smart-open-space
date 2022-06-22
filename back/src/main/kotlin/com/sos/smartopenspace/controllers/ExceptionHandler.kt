c67ea9193dbfbde5ef1078347471fa279f14ba40
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-06-22 17:05:17 -0300

    Un usuario no se puede registrar con un email que ya estaba en uso (back)

--- back/src/main/kotlin/com/sos/smartopenspace/controllers/ExceptionHandler.kt
+++ back/src/main/kotlin/com/sos/smartopenspace/controllers/ExceptionHandler.kt
@@ -1,38 +1,22 @@
 package com.sos.smartopenspace.controllers
 
-import com.sos.smartopenspace.domain.CallForPapersClosedException
-import com.sos.smartopenspace.domain.NotTheOrganizerException
-import com.sos.smartopenspace.domain.TalkIsNotForScheduledException
-import com.sos.smartopenspace.services.TalkNotFoundException
-import com.sos.smartopenspace.domain.UserDidntVoteThisTalkException
+import com.sos.smartopenspace.domain.BadRequestException
+import com.sos.smartopenspace.domain.UnprocessableEntityException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class ExceptionHandler {
 
-
-    @ExceptionHandler(
-            TalkIsNotForScheduledException::class,
-            NotTheOrganizerException::class,
-            TalkNotFoundException::class,
-            UserDidntVoteThisTalkException::class)
-    fun badRequestHandler(exception: Exception) : ResponseEntity<BadRequestException> {
-        return ResponseEntity(BadRequestException(exception.message), HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException::class)
+    fun badRequestHandler(exception: Exception) : ResponseEntity<RuntimeException> {
+        return ResponseEntity(RuntimeException(exception.message), HttpStatus.BAD_REQUEST)
     }
 
-    @ExceptionHandler(CallForPapersClosedException::class)
-    fun unprocessableEntityHandler(exception: Exception) : ResponseEntity<UnprocessableEntityException> {
-        return ResponseEntity(UnprocessableEntityException(exception.message), HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(UnprocessableEntityException::class)
+    fun unprocessableEntityHandler(exception: Exception) : ResponseEntity<RuntimeException> {
+        return ResponseEntity(RuntimeException(exception.message), HttpStatus.UNPROCESSABLE_ENTITY)
     }
-
 }
-
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class BadRequestException(message: String?) : RuntimeException(message)
-
-@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-class UnprocessableEntityException(message: String?) : RuntimeException(message)
--- front/src/helpers/api/api-client.js
+++ front/src/helpers/api/api-client.js
@@ -7,12 +7,12 @@ const doFetch = (method) => async (endpoint, body) => {
     body: JSON.stringify(body),
   };
   try {
-    const r = await window.fetch(`${process.env.API_URL}/${endpoint}`, config);
-    const r1 = await r.json();
-    if (r1.error) {
-      throw new Error(r1.message);
+    const response = await window.fetch(`${process.env.API_URL}/${endpoint}`, config);
+    const jsonResponse = await response.json();
+    if (!response.ok) {
+      throw new Error(jsonResponse.message);
     }
-    return r1;
+    return jsonResponse;
   } catch (e) {
     console.error(e);
     // const errorMsg = err.response ? err.response.data.message : 'Oops, ocurrió un error!';
