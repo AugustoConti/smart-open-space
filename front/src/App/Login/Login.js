0fd1b22f68ce95c6e51001d53961881939c81385
resLotus <naguvaris@gmail.com>
2023-01-27 09:22:14 -0300

    add password confirmation to register

--- front/src/App/Login/Login.js
+++ front/src/App/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Heading } from 'grommet';
 
 import useAuth, { useUser } from '#helpers/useAuth';
@@ -34,11 +34,21 @@ const Login = ({ location: { state } }) => {
     primaryLabel: isRegister ? 'Registrarme' : 'Ingresar',
     action: isRegister ? register : login,
   };
+  let passwordValidation = '';
+
   const onSubmit = ({ value: userData }) => {
     return data.action(userData).then(pushToRoute);
   };
 
   if (useUser()) return <RedirectToRoot />;
+
+  const validateConfirm = (value) => {
+    return value != passwordValidation && 'Las contraseñas deben coincidir';
+  };
+
+  const updatePasswordConfirm = (event) => {
+    passwordValidation = event.target.value;
+  };
 
   return (
     <MyForm
@@ -46,12 +56,13 @@ const Login = ({ location: { state } }) => {
       primaryLabel={data.primaryLabel}
       secondaryLabel={data.secondaryLabel}
       onSubmit={onSubmit}
-      value={{ name: '', email: '', password: '' }}
+      value={{ name: '', email: '', password: '', confirmPassword: '' }}
     >
       <Heading>{data.title}</Heading>
       {isRegister && <MyForm.Text />}
       <MyForm.Email />
-      <MyForm.Password />
+      <MyForm.Password onChange={updatePasswordConfirm} />
+      {isRegister && <MyForm.ConfirmPassword validate={validateConfirm} />}
     </MyForm>
   );
 };
